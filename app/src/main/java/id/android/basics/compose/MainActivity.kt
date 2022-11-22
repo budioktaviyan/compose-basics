842355b80bf338ca976dda2a43f880079baa7448
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-11-22 09:26:50 +0700

    Learn - Lists and Animations

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +17,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -37,12 +45,7 @@ class MainActivity : ComponentActivity() {
         Surface(
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background) {
-          Greeting(
-            profile = Profile(
-              "Budi Oktaviyan",
-              "GDE Android"
-            )
-          )
+          People(profiles = peopleSamples())
         }
       }
     }
@@ -68,7 +71,13 @@ fun Greeting(profile: Profile) {
 
     Spacer(modifier = Modifier.width(8.dp))
 
-    Column {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    val surfaceColor by animateColorAsState(
+      if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+    )
+
+    Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
       Text(
         text = "Hi ${profile.name}!",
         color = MaterialTheme.colors.secondaryVariant,
@@ -79,12 +88,34 @@ fun Greeting(profile: Profile) {
 
       Surface(
         shape = MaterialTheme.shapes.medium,
-        elevation = 1.dp) {
+        elevation = 1.dp,
+        color = surfaceColor,
+        modifier = Modifier
+          .animateContentSize()
+          .padding(1.dp)) {
         Text(
           text = profile.title,
           modifier = Modifier.padding(all = 4.dp),
+          maxLines = if (isExpanded) Int.MAX_VALUE else 1,
           style = MaterialTheme.typography.body2
         )
+      }
+    }
+  }
+}
+
+fun peopleSamples(): List<Profile> = listOf(
+  Profile("Sidiq Permana", "GDE Android 1"),
+  Profile("Andrew Kurniadi", "GDE Android 2"),
+  Profile("Budi Oktaviyan", "GDE Android 3")
+)
+
+@Composable
+fun People(profiles: List<Profile>) {
+  LazyColumn {
+    profiles.map { profile ->
+      item {
+        Greeting(profile)
       }
     }
   }
@@ -105,5 +136,13 @@ fun DefaultPreview() {
         "GDE Android"
       )
     )
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPeople() {
+  ComposerTheme {
+    People(profiles = peopleSamples())
   }
 }
