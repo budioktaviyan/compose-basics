456c678b4255d6df739b34650391dd4cce6afb18
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-11-22 08:39:39 +0700

    Learn - Layouts

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -3,13 +3,25 @@ package id.android.basics.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.android.basics.compose.ui.theme.ComposerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,22 +33,51 @@ class MainActivity : ComponentActivity() {
       ComposerTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Jetpack Compose")
+          Greeting(
+            profile = Profile(
+              "Budi Oktaviyan",
+              "GDE Android"
+            )
+          )
         }
       }
     }
   }
 }
 
+data class Profile(
+  val name: String,
+  val title: String
+)
+
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hi $name!")
+fun Greeting(profile: Profile) {
+  Row(modifier = Modifier.padding(all = 8.dp)) {
+    Image(
+      painter = painterResource(R.drawable.profile_picture),
+      contentDescription = "Contact Profile Picture",
+      modifier = Modifier
+        .size(40.dp)
+        .clip(CircleShape)
+    )
+    Spacer(modifier = Modifier.width(8.dp))
+    Column {
+      Text(text = "Hi ${profile.name}!")
+      Spacer(modifier = Modifier.height(4.dp))
+      Text(text = profile.title)
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   ComposerTheme {
-    Greeting("Jetpack Compose")
+    Greeting(
+      profile = Profile(
+        "Budi Oktaviyan",
+        "GDE Android"
+      )
+    )
   }
 }
