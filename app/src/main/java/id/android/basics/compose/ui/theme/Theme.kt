2330fb2533b6b8571ea0cd7deaba6b3db7ee43a1
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-11-22 08:13:16 +0700

    Add support dark mode

--- app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
   primary = Purple200,
@@ -33,6 +36,14 @@ fun ComposerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
     DarkColorPalette
   } else {
     LightColorPalette
+  }
+
+  val systemUiController = rememberSystemUiController()
+  SideEffect {
+    systemUiController.setStatusBarColor(
+      color = if (darkTheme) Color.Black else Color.White,
+      darkIcons = !darkTheme
+    )
   }
 
   MaterialTheme(
