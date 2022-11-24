db1d8d94d0a100b94d5c1654a1d18803a31800e2
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-11-24 23:36:06 +0700

    Codelabs - Start

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -4,32 +4,17 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import id.android.basics.compose.ui.theme.ComposerTheme
@@ -41,108 +26,65 @@ class MainActivity : ComponentActivity() {
 
     setContent {
       ComposerTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background) {
-          People(profiles = peopleSamples())
-        }
+        MyApp(modifier = Modifier.fillMaxSize())
       }
     }
   }
 }
 
-data class Profile(
-  val name: String,
-  val title: String
-)
+@Composable
+private fun MyApp(
+  modifier: Modifier = Modifier,
+  names: List<String> = listOf("World", "Compose")) {
+  Surface(color = MaterialTheme.colors.background) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+      for (name in names) {
+        Greeting(name = name)
+      }
+    }
+  }
+}
 
 @Composable
-fun Greeting(profile: Profile) {
-  Row(modifier = Modifier.padding(all = 8.dp)) {
-    Image(
-      painter = painterResource(R.drawable.profile_picture),
-      contentDescription = "Contact Profile Picture",
-      modifier = Modifier
-        .size(40.dp)
-        .clip(CircleShape)
-        .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-    )
+private fun Greeting(name: String) {
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
 
-    Spacer(modifier = Modifier.width(8.dp))
-
-    var isExpanded by remember { mutableStateOf(false) }
-
-    val surfaceColor by animateColorAsState(
-      if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-    )
-
-    Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-      Text(
-        text = "Hi ${profile.name}!",
-        color = MaterialTheme.colors.secondaryVariant,
-        style = MaterialTheme.typography.subtitle2
-      )
-
-      Spacer(modifier = Modifier.height(4.dp))
-
-      Surface(
-        shape = MaterialTheme.shapes.medium,
-        elevation = 1.dp,
-        color = surfaceColor,
-        modifier = Modifier
-          .animateContentSize()
-          .padding(1.dp)) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier.weight(1f)) {
         Text(
-          text = profile.title,
-          modifier = Modifier.padding(all = 4.dp),
-          maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-          style = MaterialTheme.typography.body2
+          text = "Hello,",
+          color = MaterialTheme.colors.secondary
+        )
+        Text(
+          text = name,
+          color = MaterialTheme.colors.secondary
         )
       }
-    }
-  }
-}
+      Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background)) {
 
-fun peopleSamples(): List<Profile> = listOf(
-  Profile("Sidiq Permana", "GDE Android 1"),
-  Profile("Andrew Kurniadi", "GDE Android 2"),
-  Profile("Budi Oktaviyan", "GDE Android 3")
-)
-
-@Composable
-fun People(profiles: List<Profile>) {
-  LazyColumn {
-    profiles.map { profile ->
-      item {
-        Greeting(profile)
+        Text(text = "Show more")
       }
     }
   }
 }
 
-@Preview(name = "Light Mode")
 @Preview(
   name = "Dark Mode",
   uiMode = Configuration.UI_MODE_NIGHT_YES,
-  showBackground = true
+  widthDp = 320
+)
+@Preview(
+  name = "Light Mode",
+  uiMode = Configuration.UI_MODE_NIGHT_NO,
+  widthDp = 320
 )
 @Composable
 fun DefaultPreview() {
   ComposerTheme {
-    Greeting(
-      profile = Profile(
-        "Budi Oktaviyan",
-        "GDE Android"
-      )
-    )
-  }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewPeople() {
-  ComposerTheme {
-    People(profiles = peopleSamples())
+    MyApp()
   }
 }
--- app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
@@ -6,32 +6,22 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = Purple500,
+  secondary = lightColors().background
 )
 
 private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200
-
-  /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+  primary = Purple200,
+  secondary = darkColors().background
 )
 
 @Composable
-fun ComposerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ComposerTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit) {
   val colors = if (darkTheme) {
     DarkColorPalette
   } else {
@@ -41,15 +31,13 @@ fun ComposerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
   val systemUiController = rememberSystemUiController()
   SideEffect {
     systemUiController.setStatusBarColor(
-      color = if (darkTheme) Color.Black else Color.White,
+      color = colors.background,
       darkIcons = !darkTheme
     )
   }
 
   MaterialTheme(
     colors = colors,
-    typography = Typography,
-    shapes = Shapes,
     content = content
   )
 }
