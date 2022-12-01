b3f10219b68fcf662d76b95531142a88d4e4c423
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-12-01 18:14:53 +0700

    Codelabs - Basic Layouts: Start

--- app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
@@ -5,45 +5,44 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-
-private val DarkColorPalette = darkColors(
-  surface = Blue,
-  onSurface = Navy,
-  primary = Navy,
-  onPrimary = Chartreuse
-)
 
 private val LightColorPalette = lightColors(
-  surface = Blue,
-  onSurface = Color.White,
-  primary = LightBlue,
-  onPrimary = Navy
+  primary = gray900,
+  secondary = rust600,
+  background = taupe100,
+  surface = Color.White.copy(alpha = .85f),
+  onPrimary = Color.White,
+  onSecondary = Color.White,
+  onBackground = taupe800,
+  onSurface = gray900.copy(alpha = 0.8f)
+)
+
+private val DarkColorPalette = darkColors(
+  primary = Color.White,
+  secondary = rust300,
+  background = gray900,
+  surface = Color.White.copy(alpha = 0.15f),
+  onPrimary = gray900,
+  onSecondary = gray900,
+  onBackground = taupe100,
+  onSurface = Color.White.copy(alpha = .8f)
 )
 
 @Composable
 fun ComposerTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable () -> Unit) {
+  content: @Composable() () -> Unit) {
   val colors = if (darkTheme) {
     DarkColorPalette
   } else {
     LightColorPalette
   }
 
-  val systemUiController = rememberSystemUiController()
-  SideEffect {
-    systemUiController.setStatusBarColor(
-      color = colors.background,
-      darkIcons = !darkTheme
-    )
-  }
-
   MaterialTheme(
     colors = colors,
-    typography = Typography,
+    typography = typography,
+    shapes = shapes,
     content = content
   )
 }
--- app/src/main/java/id/android/basics/compose/ui/theme/Type.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Type.kt
@@ -2,14 +2,66 @@ package id.android.basics.compose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import id.android.basics.compose.R
 
-val Typography = Typography(
+private val fontFamilyKulim = FontFamily(
+  listOf(
+    Font(
+      resId = R.font.kulim_park_regular
+    ),
+    Font(
+      resId = R.font.kulim_park_light,
+      weight = FontWeight.Light
+    )
+  )
+)
+
+private val fontFamilyLato = FontFamily(
+  listOf(
+    Font(
+      resId = R.font.lato_regular
+    ),
+    Font(
+      resId = R.font.lato_bold,
+      weight = FontWeight.Bold
+    )
+  )
+)
+
+val typography = Typography(
+  defaultFontFamily = fontFamilyLato,
+  h1 = TextStyle(
+    fontFamily = fontFamilyKulim,
+    fontWeight = FontWeight.Light,
+    fontSize = 28.sp,
+    letterSpacing = (1.15).sp
+  ),
+  h2 = TextStyle(
+    fontFamily = fontFamilyKulim,
+    fontSize = 15.sp,
+    letterSpacing = (1.15).sp
+  ),
+  h3 = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    letterSpacing = 0.sp
+  ),
   body1 = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 16.sp
+    fontSize = 14.sp,
+    letterSpacing = 0.sp
+  ),
+  button = TextStyle(
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    letterSpacing = (1.15).sp
+  ),
+  caption = TextStyle(
+    fontFamily = fontFamilyKulim,
+    fontSize = 12.sp,
+    letterSpacing = (1.15).sp
   )
 )
