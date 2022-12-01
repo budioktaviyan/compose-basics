48226548a121af49933ee0af169d32b56730f841
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-12-01 22:22:13 +0700

    Codelabs - Basic State: Start

--- app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
@@ -5,44 +5,41 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-private val LightColorPalette = lightColors(
-  primary = gray900,
-  secondary = rust600,
-  background = taupe100,
-  surface = Color.White.copy(alpha = .85f),
-  onPrimary = Color.White,
-  onSecondary = Color.White,
-  onBackground = taupe800,
-  onSurface = gray900.copy(alpha = 0.8f)
-)
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-  primary = Color.White,
-  secondary = rust300,
-  background = gray900,
-  surface = Color.White.copy(alpha = 0.15f),
-  onPrimary = gray900,
-  onSecondary = gray900,
-  onBackground = taupe100,
-  onSurface = Color.White.copy(alpha = .8f)
+  primary = Purple200,
+  primaryVariant = Purple700,
+  secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+  primary = Purple500,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 @Composable
 fun ComposerTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
-  content: @Composable() () -> Unit) {
+  content: @Composable () -> Unit) {
   val colors = if (darkTheme) {
     DarkColorPalette
   } else {
     LightColorPalette
   }
 
+  val systemUiController = rememberSystemUiController()
+  SideEffect {
+    systemUiController.setStatusBarColor(
+      color = colors.background,
+      darkIcons = !darkTheme
+    )
+  }
+
   MaterialTheme(
     colors = colors,
-    typography = typography,
-    shapes = shapes,
     content = content
   )
 }
