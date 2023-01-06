f30f8f269e953aa58317efadd695aaa632694b9a
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-01-06 17:11:52 +0700

    Codelabs - Basic Theming: Shape Work

--- app/src/main/java/id/android/basics/compose/Home.kt
+++ app/src/main/java/id/android/basics/compose/Home.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -159,7 +160,8 @@ fun PostItem(
     icon = {
       Image(
         painter = painterResource(id = post.imageThumbId),
-        contentDescription = null
+        contentDescription = null,
+        modifier = Modifier.clip(shape = MaterialTheme.shapes.small)
       )
   },
     text = {
