d4cb577546f6fc8b62dec22b7c53160d43ff623d
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-01-06 17:37:21 +0700

    Codelabs - Basic Theming: Final Theme

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -3,12 +3,17 @@ package id.android.basics.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
 
 class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContent {}
+    setContent {
+      Surface {
+        Home()
+      }
+    }
   }
 }
