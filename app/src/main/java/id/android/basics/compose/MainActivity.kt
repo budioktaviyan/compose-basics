92719e77aa334b859f70bdfe87f23bcf24364abb
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:55:12 +0700

    Codelabs - Compose Navigation: Start Migration to Compose Navigation

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -3,7 +3,6 @@ package id.android.basics.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -12,6 +11,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import id.android.basics.compose.ui.components.ComposerTabRow
 import id.android.basics.compose.ui.theme.ComposerTheme
 
@@ -30,6 +32,7 @@ class MainActivity : ComponentActivity() {
 fun MainApp() {
   ComposerTheme {
     var currentScreen: MainDestination by remember { mutableStateOf(Overview) }
+    val navController = rememberNavController()
 
     Scaffold(
       topBar = {
@@ -42,8 +45,20 @@ fun MainApp() {
         )
       }
     ) { innerPadding ->
-      Box(modifier = Modifier.padding(innerPadding)) {
-        currentScreen.screen()
+      NavHost(
+        navController = navController,
+        startDestination = Overview.route,
+        modifier = Modifier.padding(innerPadding)
+      ) {
+        composable(route = Overview.route) {
+          Overview.screen
+        }
+        composable(route = Accounts.route) {
+          Accounts.screen
+        }
+        composable(route = Bills.route) {
+          Bills.screen
+        }
       }
     }
   }
