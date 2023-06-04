4155d5a57706a1a2a7dbc52dea9e0f48edd4a4ee
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:48:07 +0700

    Codelabs - Basic Layouts: Start

--- app/src/main/java/id/android/basics/compose/ui/theme/Color.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package id.android.basics.compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Navy = Color(0xFF073042)
-val Blue = Color(0xFF4285F4)
-val LightBlue = Color(0xFFD7EFFE)
-val Chartreuse = Color(0xFFEFF7CF)
+val taupe100 = Color(0xFFF0EAE2)
+val taupe800 = Color(0xFF655454)
+val rust300 = Color(0xFFE1AFAF)
+val rust600 = Color(0xFF886363)
+val gray900 = Color(0xFF333333)
+val white150 = Color.White.copy(alpha = 0.15f)
+val white800 = Color.White.copy(alpha = 0.8f)
+val white850 = Color.White.copy(alpha = 0.85f)
+val gray800 = gray900.copy(alpha = 0.8f)
