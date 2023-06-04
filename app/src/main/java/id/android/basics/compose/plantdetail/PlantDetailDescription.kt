8e801ac90c884f3a526056c91181ff9fbaa08dce
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:54:40 +0700

    Codelabs - Basic Migration: Add AndroidView

--- app/src/main/java/id/android/basics/compose/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/id/android/basics/compose/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,8 @@
 
 package id.android.basics.compose.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -26,6 +28,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -34,6 +37,8 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import id.android.basics.compose.R
 import id.android.basics.compose.data.Plant
 import id.android.basics.compose.viewmodels.PlantDetailViewModel
@@ -53,6 +58,7 @@ fun PlantDetailContent(plant: Plant) {
         Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
             PlantName(name = plant.name)
             PlantWatering(wateringInterval = plant.wateringInterval)
+            PlantDescription(description = plant.description)
         }
     }
 }
@@ -99,6 +105,25 @@ private fun PlantWatering(wateringInterval: Int) {
     }
 }
 
+@Composable
+private fun PlantDescription(description: String) {
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(
+            description,
+            HtmlCompat.FROM_HTML_MODE_COMPACT
+        )
+    }
+
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+      },
+        update = { it.text = htmlDescription }
+    )
+}
+
 @Preview
 @Composable
 private fun PlantNamePreview() {
@@ -117,11 +142,19 @@ private fun PlantWateringPreview() {
 
 @Preview
 @Composable
+private fun PlantDescriptionPreview() {
+    MaterialTheme {
+        PlantDescription(description = "HTML<br><br>description")
+    }
+}
+
+@Preview
+@Composable
 private fun PlantDetailContentPreview() {
     val plant = Plant(
         plantId = "id",
         name = "Apple",
-        description = "description",
+        description = "HTML<br><br>description",
         growZoneNumber = 3,
         wateringInterval = 30,
         imageUrl = ""
