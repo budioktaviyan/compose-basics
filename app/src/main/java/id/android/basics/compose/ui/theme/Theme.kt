5750a1a772d4c9d473a53a873985fd78958f1b38
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:45:43 +0700

    Codelabs - Start

--- app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
+++ app/src/main/java/id/android/basics/compose/ui/theme/Theme.kt
@@ -5,40 +5,39 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-  primary = Purple200,
-  primaryVariant = Purple700,
-  secondary = Teal200
+  primary = Purple500,
+  secondary = lightColors().background
 )
 
 private val LightColorPalette = lightColors(
-  primary = Purple500,
-  primaryVariant = Purple700,
-  secondary = Teal200
-
-  /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+  primary = Purple200,
+  secondary = darkColors().background
 )
 
 @Composable
-fun ComposerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun ComposerTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit) {
   val colors = if (darkTheme) {
     DarkColorPalette
   } else {
     LightColorPalette
   }
 
+  val systemUiController = rememberSystemUiController()
+  SideEffect {
+    systemUiController.setStatusBarColor(
+      color = colors.background,
+      darkIcons = !darkTheme
+    )
+  }
+
   MaterialTheme(
     colors = colors,
-    typography = Typography,
-    shapes = Shapes,
     content = content
   )
 }
