f108f54f1a4745412011b0c3d29bb3b3932184c1
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:46:02 +0700

    Codelabs - Managing State

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -14,6 +14,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,12 +51,19 @@ private fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
+  var isExpanded by remember { mutableStateOf(false) }
+
+  val extraPadding = if (isExpanded) 48.dp else 0.dp
+
   Surface(
     color = MaterialTheme.colors.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
 
     Row(modifier = Modifier.padding(24.dp)) {
-      Column(modifier = Modifier.weight(1f)) {
+      Column(
+        modifier = Modifier
+          .weight(1f)
+          .padding(bottom = extraPadding)) {
         Text(
           text = "Hello,",
           color = MaterialTheme.colors.secondary
@@ -63,10 +74,10 @@ private fun Greeting(name: String) {
         )
       }
       Button(
-        onClick = {},
+        onClick = { isExpanded = !isExpanded },
         colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background)) {
 
-        Text(text = "Show more")
+        Text(text = if (isExpanded) "Show less" else "Show more")
       }
     }
   }
