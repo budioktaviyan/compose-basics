ad496a225a6ceb018eeae4d321a0b723c781c89b
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:50:33 +0700

    Codelabs - Basic State: State Hoisting

--- app/src/main/java/id/android/basics/compose/WellnessScreen.kt
+++ app/src/main/java/id/android/basics/compose/WellnessScreen.kt
@@ -1,9 +1,13 @@
 package id.android.basics.compose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-  WaterCounter(modifier)
+  Column(modifier = modifier) {
+    StatefulCounter()
+    WellnessTasksList()
+  }
 }
