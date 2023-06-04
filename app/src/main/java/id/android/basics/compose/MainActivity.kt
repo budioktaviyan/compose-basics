42d8d9fa911019ad3cb0d106e4051ef3fca595d2
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:50:59 +0700

    Codelabs - Basic Theming: Start

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -3,25 +3,12 @@ package id.android.basics.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import id.android.basics.compose.ui.theme.ComposerTheme
 
 class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContent {
-      ComposerTheme {
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background) {
-          WellnessScreen()
-        }
-      }
-    }
+    setContent {}
   }
 }
