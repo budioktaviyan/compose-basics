6509a857e6f5d0dc2e571acbdeb46dd3cbf0b367
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-04 12:51:49 +0700

    Codelabs - Basic Migration: Start

--- app/src/main/java/id/android/basics/compose/MainActivity.kt
+++ app/src/main/java/id/android/basics/compose/MainActivity.kt
@@ -1,19 +1,5 @@
 package id.android.basics.compose
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
 
-class MainActivity : ComponentActivity() {
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    setContent {
-      Surface {
-        Home()
-      }
-    }
-  }
-}
+class MainActivity : ComponentActivity()
