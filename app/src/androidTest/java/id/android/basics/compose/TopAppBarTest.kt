aa24479bcb84d6acc2e3d59b25fde1d7b34b1fb1
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-06-20 08:34:53 +0700

    Codelabs - Accessibility/Testing/Performance: Debugging test

--- app/src/androidTest/java/id/android/basics/compose/TopAppBarTest.kt
+++ app/src/androidTest/java/id/android/basics/compose/TopAppBarTest.kt
@@ -3,6 +3,8 @@ package id.android.basics.compose
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import id.android.basics.compose.ui.components.ComposerTopAppBar
 import id.android.basics.compose.ui.theme.ComposerTheme
 import org.junit.Rule
@@ -29,5 +31,27 @@ class TopAppBarTest {
     composeTestRule
       .onNodeWithContentDescription(ComposerScreen.Accounts.name)
       .assertIsSelected()
+  }
+
+  @Test
+  fun composerTopAppBarTest_currentLabelExists() {
+    val allScreens = ComposerScreen.values().toList()
+    composeTestRule.setContent {
+      ComposerTheme {
+        ComposerTopAppBar(
+          allScreens = allScreens,
+          onTabSelected = {},
+          currentScreen = ComposerScreen.Accounts
+        )
+      }
+    }
+
+    composeTestRule
+      .onRoot()
+      .printToLog("currentLabelExists")
+
+    composeTestRule
+      .onNodeWithContentDescription(ComposerScreen.Accounts.name)
+      .assertExists()
   }
 }
