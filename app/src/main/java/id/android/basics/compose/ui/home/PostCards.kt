93daadc2b71228542d5284b5066b11d26dfafe90
Budi Oktaviyan <budioktaviyans@gmail.com>
2023-07-11 12:00:20 +0700

    Codelabs - Accessibility: Touch Target Size

--- app/src/main/java/id/android/basics/compose/ui/home/PostCards.kt
+++ app/src/main/java/id/android/basics/compose/ui/home/PostCards.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Card
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -85,12 +86,12 @@ fun PostCardHistory(
       }
     }
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-      Icon(
-        imageVector = Icons.Default.Close,
-        contentDescription = stringResource(R.string.cd_show_fewer),
-        modifier = Modifier
-          .clickable { openDialog = true }
-          .size(24.dp))
+      IconButton(onClick = { openDialog = true }) {
+        Icon(
+          imageVector = Icons.Default.Close,
+          contentDescription = stringResource(R.string.cd_show_fewer)
+        )
+      }
     }
   }
   if (openDialog) {
